843dbde91d11026b7f6ae458f92d402664f32747
ABDUMALIK0 <88837956+ABDUMALIK0@users.noreply.github.com>
2023-05-15 18:45:01 +0900

    Delete base directory

--- base/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import PredictResult
-
-admin.site.register(PredictResult)
--- base/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import index, add_photo, add_map
-urlpatterns = [
-    path('', index, name='index'),
-    path('add_photo/', add_photo, name='add_photo'),
-    path('add_map/', add_map, name='add_map')
-]
